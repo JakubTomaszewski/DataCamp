ba1c172903be8d550629810885bc1f71dfbab95b
JakubTomaszewski <57564656+JakubTomaszewski@users.noreply.github.com>
2020-01-29 22:00:34 +0100

    Update kneighbors.py

--- kneighbors.py
+++ kneighbors.py
@@ -1,12 +1,21 @@
 # Import KNeighborsClassifier from sklearn.neighbors
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 # Create arrays for the features and the response variable
-y = df['party'].values 
-X = df.drop('party', axis=1).values
+y = df.party.values
+X = df.drop('party', axis=1).values 
 
-# Create a k-NN classifier with 6 neighbors
-knn = KNeighborsClassifier(n_neighbors=6)
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors = 6)
 
 # Fit the classifier to the data
 knn.fit(X, y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+print('The accuracy is: {}'.format(accuracy_score(y, y_pred)))
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
