e4bec8a63299a29db173c937b2abc133cf775572
Jakub Tomaszewski <57564656+JakubTomaszewski@users.noreply.github.com>
2020-02-07 12:52:34 +0100

    Update GridSearchCV.py

--- GridSearchCV.py
+++ GridSearchCV.py
@@ -1,21 +1,24 @@
 # Import necessary modules
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-import numpy as np
 
-# Setup the hyperparameter grid
+# Create the hyperparameter grid
 c_space = np.logspace(-5, 8, 15)
-param_grid = {'C': c_space}
 
-# Instantiate a logistic regression classifier: logreg
+param_grid = {'C': c_space, 'penalty': ['l1', 'l2']}
+
+# Instantiate the logistic regression classifier: logreg
 logreg = LogisticRegression()
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Instantiate the GridSearchCV object: logreg_cv
 logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
 
-# Fit it to the data
-logreg_cv.fit(X, y)
+# Fit it to the training data
+logreg_cv.fit(X_train, y_train)
 
-# Print the tuned parameters and score
-print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_)) 
-print("Best score is {}".format(logreg_cv.best_score_))
+# Print the optimal parameters and best score
+print("Tuned Logistic Regression Parameter: {}".format(logreg_cv.best_params_))
+print("Tuned Logistic Regression Accuracy: {}".format(logreg_cv.best_score_))
